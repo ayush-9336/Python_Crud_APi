3a82d825f320073f02c23a2f9535ed993e74b58a
ayush <ayushs110196@gmail.com>
2023-09-12 11:59:11 +0530

    chnages

--- employeeapi/models.py
+++ employeeapi/models.py
@@ -5,8 +5,7 @@ from django.db import models
 class Employee(models.Model):
     fullname = models.CharField(max_length=100)
     emp_code = models.CharField(max_length=3)
-    mobile = models.CharField(max_length=15)
-    
+    mobile = models.CharField(max_length=15) 
 
     # Create / Insert / Add - POST
     # Retrieve /  Fetch - GET
