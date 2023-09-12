1415f83fe347080af780d22f6f2da718cf2241f0
ayush <ayushs110196@gmail.com>
2023-09-12 11:29:08 +0530

    changes

--- employeeapi/models.py
+++ employeeapi/models.py
@@ -6,6 +6,7 @@ class Employee(models.Model):
     fullname = models.CharField(max_length=100)
     emp_code = models.CharField(max_length=3)
     mobile = models.CharField(max_length=15)
+    
 
     # Create / Insert / Add - POST
     # Retrieve /  Fetch - GET
